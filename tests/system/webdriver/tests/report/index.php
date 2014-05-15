62dc3473dec5b5408bc5d14bd588e345ede0e42a
javier gomez <javier.gomez.corio@gmail.com>
2014-05-15 20:46:17 +0200

    Fix wrong link

--- tests/system/webdriver/tests/report/index.php
+++ tests/system/webdriver/tests/report/index.php
@@ -33,16 +33,16 @@ if (file_exists('../logs/junit.xml'))
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1\">
-		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
-		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+		<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+		<script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	</head>
 	<body>
 		<?php if (!$logFileExist) : ?>
 			<h1>Please execute System Test and wait until the execution has finished to view the report</h1>
 			<h2>For more details about how to run system tests please read here
-				<a href="https://redweb.atlassian.net/wiki/display/RSBTB/2013/12/06/Running+System+Tests%2C+Automation+Testing+for+RedshopB2B"> System Test</a>
+				<a href="http://docs.joomla.org/System_Testing"> System Test</a>
 			</h2>
 		<?php else : ?>
 		<table class="table table-striped table-hover">
