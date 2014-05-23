4493b6904a340410de6ad1e671fdf19ab6dcdb0b
kshitij sharma <ksh.anand2012@gmail.com>
2014-05-23 19:00:30 +0530

    Create MenuItemsManager0002Test.php

---
+++ tests/system/webdriver/tests/menus/MenuItemsManager0002Test.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package     Joomla.Test
+ * @subpackage  Webdriver
+ *
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+require_once 'JoomlaWebdriverTestCase.php';
+
+use SeleniumClient\By;
+
+/**
+ * This class tests the Article: Front End and Add/Edit Screens.
+ *
+ * @package    Joomla.Test
+ * @subpackage Webdriver
+ * @since      3.2
+ */
+class MenuItemsManager0002Test extends JoomlaWebdriverTestCase{
+
+    /**
+	 * The page class being tested.
+	 *
+	 * @var ArticleManagerPage
+	 * @var $categoryManagerPage
+	 * @var $menuItemsManagerPage
+	 * @since 3.2
+	 */
+}
