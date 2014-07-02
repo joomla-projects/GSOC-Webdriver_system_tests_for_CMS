955d90e8ecdc09637298d1cb5725abd3fc304360
kshitij sharma <ksh.anand2012@gmail.com>
2014-07-02 15:38:21 +0530

    Update ContactManager0002Test.php

--- tests/system/webdriver/tests/components/ContactManager0002Test.php
+++ tests/system/webdriver/tests/components/ContactManager0002Test.php
@@ -24,8 +24,7 @@ use SeleniumClient\DesiredCapabilities;
  */
 class ContactManager0002Test extends JoomlaWebdriverTestCase
 {
-
-  /**
+	/**
 	 * The page class being tested.
 	 *
 	 * @var     contactManagerPage
@@ -35,6 +34,8 @@ class ContactManager0002Test extends JoomlaWebdriverTestCase
 
 	/**
 	 * Login to back end and navigate to menu Contacts.
+	 *
+	 * @return void
 	 *
 	 * @since   3.2
 	 */
@@ -48,6 +49,8 @@ class ContactManager0002Test extends JoomlaWebdriverTestCase
 	/**
 	 * Logout and close test.
 	 *
+	 * @return void
+	 *
 	 * @since   3.2
 	 */
 	public function tearDown()
@@ -57,6 +60,10 @@ class ContactManager0002Test extends JoomlaWebdriverTestCase
 	}
 
 	/**
+	 * get list of filters and match it with expected IDs
+	 *
+	 * @return void
+	 *
 	 * @test
 	 */
 	public function getFilters_GetListOfFilters_ShouldMatchExpected()
@@ -67,6 +74,10 @@ class ContactManager0002Test extends JoomlaWebdriverTestCase
 	}
 
 	/**
+	 * checking the working of published and unpublished filters
+	 *
+	 * @return void
+	 *
 	 * @test
 	 */
 	public function setFilter_SetFilterValues_ShouldExecuteFilter()
@@ -85,6 +96,10 @@ class ContactManager0002Test extends JoomlaWebdriverTestCase
 	}
 
 	/**
+	 * creating two tags one published and one unpublished and the verifying its existence
+	 *
+	 * @return void
+	 *
 	 * @test
 	 */
 	public function setFilter_TestFilters_ShouldFilterContacts()
@@ -97,7 +112,6 @@ class ContactManager0002Test extends JoomlaWebdriverTestCase
 		$this->assertTrue(strpos($message, 'Contact successfully saved') >= 0, 'Contact save should return success');
 		$state = $this->contactManagerPage->getState($contactName_1);
 		$this->assertEquals('published', $state, 'Initial state should be published');
-
 
 		$this->contactManagerPage->addContact($contactName_2, false);
 		$message = $this->contactManagerPage->getAlertMessage();
@@ -118,40 +132,75 @@ class ContactManager0002Test extends JoomlaWebdriverTestCase
 		$this->contactManagerPage->trashAndDelete($contactName_1);
 		$this->contactManagerPage->trashAndDelete($contactName_2);
 	}
-	
+
 	/**
-         * create archived contacts and then verify its existence.
-         *
-         * @test
-         */
-    public function setFilter_TestFilters_ShouldFilterTags2()
-    {
-        $contactName_1 = 'Test Filter 1';
-        $contactName_2 = 'Test Filter 2';
+	 * create archived contact and then verify its existence.
+	 *
+	 * @return void
+	 *
+	 * @test
+	 */
+	public function setFilter_TestFilters_ShouldFilterTags2()
+	{
+		$contactName_1 = 'Test Filter 1';
+		$contactName_2 = 'Test Filter 2';
 
-        $this->contactManagerPage->addContact($contactName_1, false);
-        $message = $this->contactManagerPage->getAlertMessage();
-        $this->assertTrue(strpos($message, 'Contact successfully saved') >= 0, 'Contact save should return success');
-        $state = $this->contactManagerPage->getState($contactName_1);
-        $this->assertEquals('published', $state, 'Initial state should be published');
-        $this->contactManagerPage->addContact($contactName_2, false);
-        $message = $this->contactManagerPage->getAlertMessage();
-        $this->assertTrue(strpos($message, 'Contact successfully saved') >= 0, 'Contact save should return success');
-        $state = $this->contactManagerPage->getState($contactName_2);
-        $this->assertEquals('published', $state, 'Initial state should be published');
-        $this->contactManagerPage->changeContactState($contactName_2, 'Archived');
+		$this->contactManagerPage->addContact($contactName_1, false);
+		$message = $this->contactManagerPage->getAlertMessage();
+		$this->assertTrue(strpos($message, 'Contact successfully saved') >= 0, 'Contact save should return success');
+		$state = $this->contactManagerPage->getState($contactName_1);
+		$this->assertEquals('published', $state, 'Initial state should be published');
+		$this->contactManagerPage->addContact($contactName_2, false);
+		$message = $this->contactManagerPage->getAlertMessage();
+		$this->assertTrue(strpos($message, 'Contact successfully saved') >= 0, 'Contact save should return success');
+		$state = $this->contactManagerPage->getState($contactName_2);
+		$this->assertEquals('published', $state, 'Initial state should be published');
+		$this->contactManagerPage->changeContactState($contactName_2, 'Archived');
 
-        $this->contactManagerPage->setFilter('filter_published', 'Archived');
-        $this->assertFalse($this->contactManagerPage->getRowNumber($contactName_1), 'Contact should not show');
-        $this->assertGreaterThanOrEqual(1, $this->contactManagerPage->getRowNumber($contactName_2), 'Test Contact should be present');;
+		$this->contactManagerPage->setFilter('filter_published', 'Archived');
+		$this->assertFalse($this->contactManagerPage->getRowNumber($contactName_1), 'Contact should not show');
+		$this->assertGreaterThanOrEqual(1, $this->contactManagerPage->getRowNumber($contactName_2), 'Test Contact should be present');
 
-        $this->contactManagerPage->setFilter('filter_published', 'Published');
-        $this->assertFalse($this->contactManagerPage->getRowNumber($contactName_2), 'Contact should not show');
-        $this->assertGreaterThanOrEqual(1, $this->contactManagerPage->getRowNumber($contactName_1), 'Test Contact should be present');
-        $this->contactManagerPage->setFilter('Select Status', 'Select Status');
-        $this->contactManagerPage->trashAndDelete($contactName_1);
-        $this->contactManagerPage->trashAndDelete($contactName_2);
-    }
+		$this->contactManagerPage->setFilter('filter_published', 'Published');
+		$this->assertFalse($this->contactManagerPage->getRowNumber($contactName_2), 'Contact should not show');
+		$this->assertGreaterThanOrEqual(1, $this->contactManagerPage->getRowNumber($contactName_1), 'Test Contact should be present');
+		$this->contactManagerPage->setFilter('Select Status', 'Select Status');
+		$this->contactManagerPage->trashAndDelete($contactName_1);
+		$this->contactManagerPage->trashAndDelete($contactName_2);
+	}
 
+	/**
+	 * check and verify the creation of the featured contact
+	 *
+	 * @return void
+	 *
+	 * @test
+	 */
+	public function addContact_FeaturedContact_ContactAdded()
+	{
+		$cfg = new SeleniumConfig;
+		/*adding contact of the test contact*/
+		$contactManager = 'administrator/index.php?option=com_contact';
+		$this->driver->get($cfg->host . $cfg->path . $contactManager);
+		$salt = rand();
+		$contactName = 'contact_ABC' . $salt;
+		$this->contactManagerPage = $this->getPageObject('ContactManagerPage');
+		$this->assertFalse($this->contactManagerPage->getRowNumber($contactName), 'Test contact should not be present');
+		$this->contactManagerPage->addContact($contactName, array('Featured' => 'Yes'));
+		$message = $this->contactManagerPage->getAlertMessage();
+		$this->assertTrue(strpos($message, 'Contact successfully saved') >= 0, 'contact save should return success');
 
+		/*confirming if the contact is present in front end*/
+		$PageUrl = 'index.php/using-joomla/extensions/components/contact-component/featured-contacts';
+		$this->driver->get($cfg->host . $cfg->path . $PageUrl);
+		$this->siteHomePage = $this->getPageObject('SiteContentFeaturedPage');
+		$this->assertTrue($this->siteHomePage->itemExist($contactName, 'a'), 'contact should be present');
+
+		/*delete test contact*/
+		$cpPage = $this->doAdminLogin();
+		$this->driver->get($cfg->host . $cfg->path . $contactManager);
+		$this->contactManagerPage = $this->getPageObject('ContactManagerPage');
+		$this->contactManagerPage->trashAndDelete($contactName);
+		$this->assertFalse($this->contactManagerPage->getRowNumber($contactName), 'Test article should not be present');
+	}
 }
