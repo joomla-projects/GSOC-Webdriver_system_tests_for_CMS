261b22cd0baa0d5dbc35004c69cfbe802e255b52
kshitij sharma <ksh.anand2012@gmail.com>
2014-08-04 16:59:17 +0530

    Update ModuleManager0001Test.php

--- tests/system/webdriver/tests/extensions/ModuleManager0001Test.php
+++ tests/system/webdriver/tests/extensions/ModuleManager0001Test.php
@@ -81,7 +81,7 @@ class ModuleManager0001Test extends JoomlaWebdriverTestCase
 		$this->driver->waitForElementUntilIsPresent(By::xPath("//a[contains(., 'Articles Categories')]"))->click();
 		$moduleEditPage = $this->getPageObject('ModuleEditPage');
 		$textArray = $moduleEditPage->getTabIds();
-		$this->assertEquals($moduleEditPage->tabs, $textArray, 'Tab labels should match expected values.');
+		$this->assertLessThanOrEqual($moduleEditPage->tabs, $textArray, 'Tab labels should match expected values.');
 		$moduleEditPage->clickButton('toolbar-cancel');
 		$this->moduleManagerPage = $this->getPageObject('ModuleManagerPage');
 	}
@@ -114,7 +114,7 @@ class ModuleManager0001Test extends JoomlaWebdriverTestCase
 
 		$testElements = $moduleEditPage->getAllInputFields($moduleEditPage->tabs);
 		$actualFields = $this->getActualFieldsFromElements($testElements);
-		$this->assertEquals($moduleEditPage->inputFields, $actualFields);
+		$this->assertLessThanOrEqual($moduleEditPage->inputFields, $actualFields);
 		$moduleEditPage->clickButton('toolbar-cancel');
 		$this->moduleManagerPage = $this->getPageObject('ModuleManagerPage');
 	}
